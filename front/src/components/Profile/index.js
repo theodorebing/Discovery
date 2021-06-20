a558b3d35a0e3c5484c72540fdf9e3a652c6397d
Théodore Bing <theodorebing@gmail.com>
2021-06-20 21:36:36 +0200

    first draw of profile page

--- front/src/components/Profile/index.js
+++ front/src/components/Profile/index.js
@@ -4,12 +4,14 @@ import PropTypes from 'prop-types';
 import './styles.scss';
 import Page from '../../containers/Page';
 import LinkForm from '../../containers/LinkForm';
+import ProfileForm from '../ProfileForm';
 
 const Profile = () => (
   <Page>
     <div className="profile">
       <LinkForm />
       <h2 className="profile__header">PROFILE</h2>
+      <ProfileForm />
     </div>
   </Page>
 );
---
+++ front/src/components/ProfileForm/index.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+import './styles.scss';
+
+const ProfileForm = () => (
+
+  <form action="" className="form form-form profile" onSubmit={() => {}}>
+    <div className="profile__container">
+      <p>
+        Name : Théodore Bing
+      </p>
+      <p>
+        Change your name
+      </p>
+      <p>
+        Email : theodorebing@gmail.com
+      </p>
+      <p>
+        Change your email
+      </p>
+      <p>
+        Change your password
+      </p>
+    </div>
+  </form>
+);
+
+export default ProfileForm;
