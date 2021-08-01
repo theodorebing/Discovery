9291022150b06e2cb0ca8dd154626a20d6c145f4
Théodore Bing <theodorebing@gmail.com>
2021-08-01 18:48:53 +0200

    switched to new branch

--- front/src/components/Profile/index.js
+++ front/src/components/Profile/index.js
@@ -1,18 +1,45 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import PropTypes from 'prop-types';
+import axios from '../../api';
 
 import './styles.scss';
 import Page from '../../containers/Page';
 import LinkForm from '../../containers/LinkForm';
 
-const Profile = () => (
-  <Page>
-    <div className="profile">
-      <LinkForm />
-      <h2 className="profile__header">PROFILE</h2>
-    </div>
-  </Page>
-);
+const Profile = () => {
+  const [user, setUser] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
+
+  useEffect(() => {
+    axios.get('account')
+      .then((response) => {
+        setUser(response.data);
+      })
+      .catch((error) => {
+        setErrorMessage(error);
+      });
+  }, []);
+  return (
+    <Page>
+      <div className="profile">
+        <LinkForm />
+
+        <h2 className="profile__header">PROFILE</h2>
+        {!errorMessage && user && (
+        <>
+          <h3 className="profile__name">{user.name}</h3>
+          <h3 className="profile__name">{user.email}</h3>
+          <h3 className="profile__name">{user.created_at}</h3>
+          <h3 className="profile__name">{user.updated_at}</h3>
+        </>
+        )}
+        {errorMessage && (
+          <p className="profile__errorMessage">{errorMessage}</p>
+        )}
+      </div>
+    </Page>
+  );
+};
 
 Page.propTypes = {
   children: PropTypes.node.isRequired,
